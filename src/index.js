5f26219d0e9fe6690f4cccc6052a212452f49ccd
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-17 10:49:01 -0700

    Clean up non-working and extra code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import $ from 'jquery'
 
-// var apiCalls = require('./apiCalls')
-// var topWord = apiCalls.topWord()
-
 $(document).ready(() => {
-  // var article = document.getElementsByClassName('word-count')[0]
-  // var newParagraph = document.createElement("p")
-  // var newText = document.createTextNode(`${topWord}`)
-  //
-  // article.appendChild(newParagraph.appendChild(newText))
-
   $.ajax({
-               url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
-               type: 'get',
-               success: function(data){
-                   // var article = document.getElementsByClassName('word-count')[0]
-                   // var newParagraph = document.createElement("p")
-                   // var newText = document.createTextNode(`${data.word}`)
-                   // console.log(data.word)
-                   // article.appendChild(newParagraph.appendChild(newText))
-
-                   // If the success function is execute,
-                   // then the Ajax request was successful.
-                   // Add the data we received in our Ajax
-                   // request to the "content" div.
-                   $('#this-one').text(JSON.stringify(data.word));
-               }
-           });
+     url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
+     type: 'get',
+     success: function(data){
+         $('#this-one').text(JSON.stringify(data.word));
+     }
+  });
 })
