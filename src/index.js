8170db38264305d0599fca65a2d27501b8eeae9f
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-17 11:47:25 -0700

    Start process of not having to refresh the page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  $.ajax({
-     url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
-     type: 'get',
-     success: function(data){
-         $('#this-one').text(JSON.stringify(data.word));
-     }
+  document.addEventListener("mousemove", () => {
+    $.ajax({
+       url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
+       type: 'get',
+       success: function(data){
+         var word = JSON.stringify(data.word)
+         $('#this-one').text(word);
+       }
+    });
   });
-
 
   $("button").click(() => {
     var array = document.getElementsByTagName('textarea')[0].value.split(" ")
@@ -19,7 +21,6 @@ $(document).ready(() => {
          type: 'post',
          data: json,
          success: function(msg){
-
          }
       });
     })
