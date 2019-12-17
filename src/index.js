78c2a085cdda4ac93cd03055b1b90f70591058e9
Tyler Schaffer <tpschaffer@gmail.com>
2019-12-17 11:03:33 -0700

    Post a single hard-coded word with ajax

--- src/index.js
+++ src/index.js
@@ -8,4 +8,16 @@ $(document).ready(() => {
          $('#this-one').text(JSON.stringify(data.word));
      }
   });
+
+  var array = ["amazing"]
+  var json = {"word": {"value": "chicken"}}
+
+  $.ajax({
+     url: 'https://wordwatch-api.herokuapp.com/api/v1/words',
+     type: 'post',
+     data: json,
+     success: function(msg){
+
+     }
+  });
 })
